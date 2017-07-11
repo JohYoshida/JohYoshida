956badd4c0807411bdcd00ba6f8d2a91a3df823e
Joh Yoshida <johyoshida@gmail.com>
2017-07-11 14:41:46 -0700

    Set up app

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict"
+const PORT = process.env.PORT || 8080;
+
+// Requiring Express and server dependencies
+const express = require("express");
+const ejs = require("ejs");
+const ejs = require("ejs");
+
+// Setting app to use express
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// Start server
+app.listen(PORT, process.env.IP, function(){
+  console.log("Server has started and is running on port:", PORT);
+});
